f200bba250b107f94120fcfe07d96014f51530a9
Robert <robert.3dgamedev@gmail.com>
2023-08-28 13:26:42 +0200

    added themes

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,5 +5,19 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.everforest" },
+  { import = "astrocommunity.colorscheme.sonokai" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  {
+    "sonokai",
+    init = function ()
+      vim.g.sonokai_style = "maia" -- "maia" "default" "shusia" "espresso"
+    end
+  },
+  {
+    "everforest",
+    init = function()
+      vim.g.everforest_background = "hard"
+    end
+  },
 }
